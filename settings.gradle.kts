b9a651a27dd2c12cc897ff1a4dc6469a28aeecba
unknown <ouardijamal87@gmail.com>
2023-11-11 12:32:51 +0100

    msg

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "M206_sqlite_3"
+include(":app")
+ 
